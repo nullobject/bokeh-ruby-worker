35d54c17cfc1f7097b163e4f7f58c87c8c6b2f7e
Josh Bassett <josh.bassett@gmail.com>
2014-06-27 13:49:00 +1000

    Initial import.

---
+++ worker.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require 'ffi-rzmq'
+require 'json'
+require 'ostruct'
+
+if ARGV.length < 1
+  puts 'usage: ruby worker.rb <connect-to>'
+  exit
+end
+
+connect_to = ARGV[0]
+
+ctx = ZMQ::Context.new
+s = ctx.socket(ZMQ::REP)
+s.connect(connect_to)
+
+json = ''
+
+while true do
+  s.recv_string(json)
+
+  if json.length > 0
+    request = OpenStruct.new(JSON.parse(json))
+    puts request
+
+    response = OpenStruct.new(id: request.id, response: 'completed', data: request.data.reverse)
+    puts response
+
+    json = JSON.dump(response.marshal_dump)
+    s.send_string(json, 0)
+  end
+end
